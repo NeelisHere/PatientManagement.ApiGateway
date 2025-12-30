5fcb527c9cabd376d8d5bbda8be800680f60d83d
Neelabh Paul <npaul.ibmoffice@ibm.com>
2025-12-29 21:31:21 -0800

    added aws cloudmap service urls

--- src/main/java/com/patientManagement/api_gateway/filter/JwtValidationGatewayFilterFactory.java
+++ src/main/java/com/patientManagement/api_gateway/filter/JwtValidationGatewayFilterFactory.java
@@ -13,7 +13,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
     private final WebClient webClient;
 
-    public JwtValidationGatewayFilterFactory(@Value("${auth-service.url}") String authServiceUrl) {
+    public JwtValidationGatewayFilterFactory(@Value("${authservice.url}") String authServiceUrl) {
         this.webClient = WebClient.builder().baseUrl(authServiceUrl).build();
     }
 
